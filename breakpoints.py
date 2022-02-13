741f9fe02c1693a9d5705e57430751c06b080b19
Sami El-Daher <samisamer05@hotmail.co.uk>
2022-02-13 16:44:51 +0000

    use optimised version for breakpoints

--- breakpoints.py
+++ breakpoints.py
@@ -1,16 +1,18 @@
+import gdb
+
 class LispBreakpoint(gdb.Breakpoint):
-    def __init__(self, func_name: str, c_func: str):
-        super().__init__(c_func)
+    def __init__(self, func_name: str, c_func_name: str):
+        assert CFunctions.cool_func(c_func_name)
 
         self.func_name = func_name
-        self.c_func = c_func
+        self.c_func_name = c_func_name
+        self.c_func = CFunctions(c_func_name).wrapper()
+
+        print(f"set breakpoint: {self.func_name}[@{self.c_func_name}:{self.c_func}]")
+
+        super().__init__(c_func_name)
 
     def stop(self):
         frame = gdb.newest_frame()
-        assert frame.name() == self.c_func
-        assert CFunctions.cool_func(self.c_func)
 
-        func_class = CFunctions(self.c_func).wrapper()
-        self.func = func_class(frame)
-
-        return self.func.name() == self.func_name
+        return self.c_func.check_name(self.func_name)
