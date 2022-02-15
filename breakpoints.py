f6b3072b4f598939b66943a6ba4b26583d027167
Sami El-Daher <samisamer05@hotmail.co.uk>
2022-02-15 11:19:30 +0000

    minor pretty printing tweaks

--- breakpoints.py
+++ breakpoints.py
@@ -8,7 +8,7 @@ class LispBreakpoint(gdb.Breakpoint):
         self.c_func_name = c_func_name
         self.c_func = CFunctions(c_func_name).wrapper()
 
-        print(f"set breakpoint: {self.func_name}[@{self.c_func_name}:{self.c_func}]")
+        print(f"set breakpoint: {self}")
 
         super().__init__(c_func_name)
 
@@ -16,3 +16,6 @@ class LispBreakpoint(gdb.Breakpoint):
         frame = gdb.newest_frame()
 
         return self.c_func.check_name(self.func_name)
+
+    def __str__(self):
+        return f"{self.func_name} [in {self.c_func_name}]"
